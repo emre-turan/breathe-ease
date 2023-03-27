690981fa54d7a2081965233dbbd8a6dcbb64563d
Emre Turan <itsemreturan@gmail.com>
2023-03-27 19:45:22 +0300

    updated favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
+       <Head>
+       <link rel="icon" href="/emre-logo-dark-copy.svg" />
+      </Head>
       <Component {...pageProps} />
     </main>
   );
