714c76d294e944f998dc8b76fb39da3e2e4e39c0
Emre Turan <itsemreturan@gmail.com>
2023-04-06 18:39:49 +0300

    vercel analytics config

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
 import "@/styles/globals.css";
-import { Inter } from 'next/font/google';
-import Head from 'next/head';
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
-       <Head>
-       <link rel="icon" href="/emre-logo-dark-copy.svg" />
+      <Head>
+        <link rel="icon" href="/emre-logo-dark-copy.svg" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
